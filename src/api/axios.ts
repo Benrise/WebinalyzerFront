963c8397d61c667ea852298feebd46f23dbc19a6
Benrise <benrise_x@mail.ru>
2024-04-27 22:46:20 +0300

    Axios interceptors fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,9 @@
 import { API_BASE_URL } from '@/shared/config';
 import axios from 'axios';
+import { type InternalAxiosRequestConfig, type AxiosResponse, AxiosError } from "axios"
+import { useToast } from '@/shared/ui/toast/use-toast'
+
+const { toast } = useToast();
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -8,5 +12,32 @@ const axiosInstance = axios.create({
   },
   timeout: 100000,
 })
+
+axiosInstance.interceptors.request.use(
+  (config): InternalAxiosRequestConfig => {
+      return config;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+      alert()
+      return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  async (response): Promise<AxiosResponse> => { 
+    return Promise.resolve(response);
+  },
+  async (error: AxiosError) => {
+    const statusCode = error.response?.status;
+    console.error(error);
+    if (statusCode && statusCode >= 500) {
+        toast({
+          title: `${statusCode} - сервер недоступен`,
+          description: 'Сервер по техническим причинам временно не может обрабатывать запросы',
+        });
+    }
+    Promise.reject(error);
+  },
+);
 
 export default axiosInstance
